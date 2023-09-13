465901e409e172c48ce3fa07ee9ca38c4dab563d
Yiwei Shen <a1829951@student.adelaide.edu.au>
2023-09-13 21:17:50 +0930

    Set up Firebase database and store the input value to the database

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSetting = {
+  databaseURL:
+    "https://shoppinglist-6f808-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+const app = initializeApp(appSetting);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "ShoppingList");
 const inputField = document.getElementById("input-field");
 const addButton = document.getElementById("add-button");
 
 addButton.addEventListener("click", function () {
+  let inputValue = inputField.value;
+  push(shoppingListInDB, inputValue);
   console.log("it works");
 });
